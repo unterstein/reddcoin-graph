bd2df41ce851b7458160dd5cb280c601f56dd96f
Johannes Unterstein <unterstein@me.com>
2018-01-16 21:10:47 +0100

    added command line arguments to neo4j exporter

--- src/main/java/io/github/unterstein/NeoExporter.java
+++ src/main/java/io/github/unterstein/NeoExporter.java
@@ -5,19 +5,33 @@ import org.neo4j.driver.v1.AuthTokens;
 import org.neo4j.driver.v1.Driver;
 import org.neo4j.driver.v1.GraphDatabase;
 import org.neo4j.driver.v1.Session;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Export data from local data structure to neo4j
+ */
 public class NeoExporter {
 
+  private static final Logger log = LoggerFactory.getLogger(TwitterImport.class);
+
   public static void main(String[] args) throws IOException {
+    // do rudimentary input arg validation
+    if (args.length != 3) {
+      log.error("Please provide the following twitter api information in this order:");
+      log.error("$neo4jUri $neo4jUser $neo4jPassword");
+      System.exit(1);
+    }
     String[] lines = FileUtils.readFileToString(new File("output.txt"), "UTF-8").split(System.lineSeparator());
     List<String> userNames = new ArrayList<>();
     List<TipEntry> entries = new ArrayList<>();
 
+    // build internal data structure from input file
     for (String line : lines) {
       String[] split = line.split(":");
       if (!userNames.contains(split[0])) {
@@ -29,11 +43,14 @@ public class NeoExporter {
       entries.add(new TipEntry(userNames.indexOf(split[0]), userNames.indexOf(split[1]), Integer.valueOf(split[2])));
     }
 
-    Driver neo4jDriver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "neo4j123"));
+    // setup neo4j driver
+    Driver neo4jDriver = GraphDatabase.driver(args[0], AuthTokens.basic(args[1], args[2]));
     Session session = neo4jDriver.session();
 
+    // delete previous nodes
     session.run("MATCH (u:User) DETACH DELETE u;");
 
+    // create graph user nodes from known users
     String createUsers = "CREATE ";
     String separator = "";
     for (String userName : userNames) {
@@ -42,11 +59,15 @@ public class NeoExporter {
     }
     session.run(createUsers);
 
+    // create connections between users based on known tips
     for (TipEntry entry : entries) {
       session.run("MATCH (sender:User {id: " + entry.sender + "}), (receiver:User {id: " + entry.receiver + "}) MERGE (sender)-[r:TIPS { amount: " + entry.amount + "}]->(receiver)");
     }
   }
 
+  /**
+   * Internal data structure to model a tip from one user to another
+   */
   private static class TipEntry {
     int sender;
     int receiver;
