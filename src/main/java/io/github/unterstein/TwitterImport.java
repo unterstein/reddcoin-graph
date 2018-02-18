510d4be4817477e5a387afa4fe609b2939c2d5a9
Johannes Unterstein <johannes.unterstein@neotechnology.com>
2018-02-18 19:58:55 +0100

    change import to search

--- src/main/java/io/github/unterstein/TwitterImport.java
+++ src/main/java/io/github/unterstein/TwitterImport.java
@@ -8,6 +8,7 @@ import twitter4j.*;
 import twitter4j.auth.AccessToken;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
 
 /**
  * Import data from twitter to local data structure
@@ -15,6 +16,7 @@ import java.io.File;
 public class TwitterImport {
 
   private static final Logger log = LoggerFactory.getLogger(TwitterImport.class);
+  private static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEEE MMM dd.MM.yy HH.mm.ss zzz");
 
   public static void main(String[] args) {
     // do rudimentary input arg validation
@@ -31,31 +33,40 @@ public class TwitterImport {
 
     // search in twitter timeline
     File outputFile = new File("output.txt");
+    int i = 0;
     try {
-      boolean requestsAvailable = true;
-      for (int i = 1; i < 500; i++) {
+      Query query = new Query("\"you have received a tip\" from:tipreddcoin");
+      while (true) {
         log.info("Starting run #" + i);
-        if (requestsAvailable) {
-          ResponseList<Status> tipreddcoin = twitter.getUserTimeline("tipreddcoin", new Paging(i, 100));
-          requestsAvailable = tipreddcoin.getRateLimitStatus().getRemaining() > 0;
-          for (Status status : tipreddcoin) {
+        query.setCount(100);
+        QueryResult result = twitter.search(query);
+        if (result.getRateLimitStatus().getRemaining() == 0) {
+          int coolDown = (result.getRateLimitStatus().getSecondsUntilReset() + 2);
+          log.warn(String.format("Sleeping %d seconds to cool down rate limit", coolDown));
+          Thread.sleep(coolDown * 1000);
+        } else {
+          if (result.getTweets().size() == 0) {
+            log.info("Did not found new tweets, stopping.");
+            break;
+          }
+          for (Status status : result.getTweets()) {
             String text = status.getText();
             if (text.contains("confirmed:  -->>@")) {
-              String sender = StringUtils.substringBefore(text, " confirmed:  -->>");
-              String receiver = StringUtils.substringBetween(text, " confirmed:  -->>", " ");
+              String receiver = StringUtils.substringBefore(text, ", you have received a");
+              String sender = StringUtils.substringBetween(text, " from ", ".");
               String amount = StringUtils.substringBetween(text, "Ɍ", " Reddcoins");
-              String output = String.format("%s:%s:%s", sender, receiver, amount);
+              String date = simpleDateFormat.format(status.getCreatedAt());
+              String output = String.format("%s:%s:%s:%s", sender, receiver, amount, date);
               log.info(output);
               FileUtils.writeStringToFile(outputFile, output + System.lineSeparator(), "UTF-8", true);
             }
           }
-        } else {
-          log.info("rate limited");
         }
-        log.info("sleeping");
-        Thread.sleep(1500);
+        if (result.hasNext()) {
+          query = result.nextQuery();
+        }
+        i++;
       }
-
     } catch (Exception e) {
       log.error("Unable to perform twitter import", e);
     }
